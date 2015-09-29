5072013d9e2b67bf2c3fb57172e5ca2610c4d5ce
InvertGames <invertgamestudios@gmail.com>
2015-09-29 18:40:25 -0400

    no message

--- Inspectors/ComponentEditor.cs
+++ Inspectors/ComponentEditor.cs
@@ -1,15 +1,135 @@
-﻿using Invert.Common;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Invert.Common;
 using Invert.Core;
 using Invert.Core.GraphDesigner;
 using UnityEditor;
 using UnityEngine;
 
-[UnityEditor.CustomEditor(typeof(uFrame.ECS.EcsComponent),true)]
+[UnityEditor.CustomEditor(typeof(uFrame.ECS.EcsComponent), true)]
 public class ComponentEditor : Editor
 {
     public override void OnInspectorGUI()
     {
-        base.OnInspectorGUI();
+        //base.OnInspectorGUI();
+      
+        var properties = target.GetType().GetProperties();
+        serializedObject.Update();
+        SerializedProperty iterator = serializedObject.GetIterator();
+        for (bool enterChildren = true; iterator.NextVisible(enterChildren); enterChildren = false)
+        {    
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(iterator, true, new GUILayoutOption[0]);
+            if (EditorGUI.EndChangeCheck())
+            {
+                var propertyName = iterator.name.ToLower().Substring(1);
+                var propertyInfo = properties.FirstOrDefault(p => p.Name.ToLower() == propertyName);
+                if (propertyInfo != null)
+                {
+                    if (propertyInfo.PropertyType == typeof (int))
+                    {
+                        propertyInfo.SetValue(target, iterator.intValue,null);
+                    }
+                    if (propertyInfo.PropertyType == typeof(AnimationCurve))
+                    {
+                        propertyInfo.SetValue(target, iterator.animationCurveValue, null);
+                    }
+                    if (propertyInfo.PropertyType == typeof(Color))
+                    {
+                        propertyInfo.SetValue(target, iterator.colorValue, null);
+                    }
+                    if (propertyInfo.PropertyType == typeof(Quaternion))
+                    {
+                        propertyInfo.SetValue(target, iterator.quaternionValue, null);
+                    }
+                    if (propertyInfo.PropertyType == typeof(float))
+                    {
+                        propertyInfo.SetValue(target, iterator.floatValue, null);
+                    }
+                    if (propertyInfo.PropertyType == typeof(bool))
+                    {
+                        propertyInfo.SetValue(target, iterator.boolValue, null);
+                    }
+                    if (propertyInfo.PropertyType == typeof(Bounds))
+                    {
+                        propertyInfo.SetValue(target, iterator.boundsValue, null);
+                    }
+                    if (propertyInfo.PropertyType == typeof(string))
+                    {
+                        propertyInfo.SetValue(target, iterator.stringValue, null);
+                    }
+                    if (propertyInfo.PropertyType == typeof(Vector2))
+                    {
+                        propertyInfo.SetValue(target, iterator.vector2Value, null);
+                    }
+                    if (propertyInfo.PropertyType == typeof(Vector3))
+                    {
+                        propertyInfo.SetValue(target, iterator.vector3Value, null);
+                    }
+                    if (propertyInfo.PropertyType == typeof(Vector4))
+                    {
+                        propertyInfo.SetValue(target, iterator.vector4Value, null);
+                    }
+                    if (typeof(Enum).IsAssignableFrom(propertyInfo.PropertyType))
+                    {
+                        propertyInfo.SetValue(target, Enum.GetValues(propertyInfo.PropertyType).GetValue(iterator.enumValueIndex), null);
+                    }
+                }
+            }
+        }
+        serializedObject.ApplyModifiedProperties();
+
+     
+
+
+        //var properties = target.GetType().GetProperties();
+        //var fields = target.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+
+        //foreach (var field in fields.Where(p=>p.IsPublic || p.IsDefined(typeof(SerializeField),true)))
+        //{
+        //    var propertyName = field.Name.ToLower().Substring(1);
+        //    var propertyInfo = properties.FirstOrDefault(p => p.Name.ToLower() == propertyName);
+        //    if (propertyInfo != null)
+        //    {
+        //        if (typeof (int) == propertyInfo.PropertyType)
+        //        {
+        //            EditorGUI.BeginChangeCheck();
+        //            var value = EditorGUILayout.IntField(field.GetValue(target),)
+        //            if (EditorGUI.EndChangeCheck())
+        //            {
+
+        //            }
+        //            continue;
+        //        }
+        //        if (typeof(string) == propertyInfo.PropertyType)
+        //        {
+        //            continue;
+        //        }
+        //        if (typeof(bool) == propertyInfo.PropertyType)
+        //        {
+        //            continue;
+        //        }
+        //        if (typeof(float) == propertyInfo.PropertyType)
+        //        {
+        //            continue;
+        //        }
+        //        if (typeof(Vector3) == propertyInfo.PropertyType)
+        //        {
+        //            continue;
+        //        }
+        //        if (typeof(Enum) == propertyInfo.PropertyType)
+        //        {
+        //            continue;
+        //        }
+
+
+        //    }
+
+        //    var property = serializedObject.FindProperty(field.Name);
+        //    EditorGUILayout.PropertyField(property);
+        //}
+
         InvertApplication.SignalEvent<IDrawUnityInspector>(_ => _.DrawInspector(target));
     }
 }
